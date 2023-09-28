a9116cadcbaca7425fead93a00343cf22a07a152
bill <bill.arnold@student.moringaschool.com>
2023-09-28 11:54:30 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+// Basic Math Functions
+function add(a, b) {
+    // Implement addition
+  }
+  
+  function subtract(a, b) {
+    // Implement subtraction
+  }
+  
+  function multiply(a, b) {
+    // Implement multiplication
+  }
+  
+  function divide(a, b) {
+    // Implement division
+  }
+  
+  // Increment and Decrement Functions
+  function increment(n) {
+    // Implement increment
+  }
+  
+  function decrement(n) {
+    // Implement decrement
+  }
+  
+  // Parsing Numbers Functions
+  function makeInt(string) {
+    // Implement parsing to integer
+  }
+  
+  function preserveDecimal(string) {
+    // Implement parsing to float
+  }
+  
